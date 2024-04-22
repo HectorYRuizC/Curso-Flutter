e6a61295f733bda86241a0eb8c4b1b7cd0ed77a9
Hector <ruizcadenahector@gmail.com>
2024-04-22 15:45:10 -0500

    nuevos cambios

--- yes_no_app/lib/config/theme/app_theme.dart
+++ yes_no_app/lib/config/theme/app_theme.dart
@@ -1,26 +1,29 @@
 import 'package:flutter/material.dart';
 
-const Color _customColor = Color(0Xff5C11D4);
-const List<Color> _colorTheme = [
+const Color _customColor = Color(0xFF49149F);
+
+const List<Color> _colorThemes = [
   _customColor,
   Colors.blue,
-  Color.fromARGB(255, 51, 255, 0),
-  Colors.pink,
   Colors.teal,
+  Colors.green,
+  Colors.yellow,
   Colors.orange,
+  Colors.pink,
 ];
 
 class AppTheme {
   final int selectedColor;
 
   AppTheme({this.selectedColor = 0})
-      : assert(selectedColor >= 0 && selectedColor <= _colorTheme.length - 1,
-            'Colors must be between o and ${_colorTheme.length}');
+      : assert(selectedColor >= 0 && selectedColor <= _colorThemes.length - 1,
+            'Colors must be between 0 and ${_colorThemes.length}');
+
   ThemeData theme() {
     return ThemeData(
       useMaterial3: true,
-      colorSchemeSeed: _colorTheme[selectedColor],
-      //brightness: Brightness.dark,
+      colorSchemeSeed: _colorThemes[selectedColor],
+      // brightness: Brightness.dark
     );
   }
 }
